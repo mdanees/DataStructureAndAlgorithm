05b9b14f8bcac17e4f6f4325bd76352070839048
root <root@test.opentext.net>
2021-05-24 10:48:20 +0000

    Added new files

---
+++ fallAndCrush.cpp
@@ -0,0 +1,182 @@
+/*You are running a gravity simulation involving falling boxes and breakable obstacles. The situation is represented by two rectangular matrices of equal dimensions, board and strengths.
+
+Each cell of the matrix board contains one of three characters:
+
+    '.', which means that the cell is empty;
+    '*', which means that the cell contains an obstacle;
+    '#', which means that the cell contains a box.
+
+The matrix strengths shows the strength of each obstacle - the maximum number of boxes that can be stacked on top of the obstacle without it breaking.
+
+    If board{i}{j} = '*' then strengths{i}{j} is a positive integer;
+    Otherwise, strengths{i}{j} = 0 (if board{i}{j} is '.' or '#').
+
+The boxes each fall down as far as possible, until hitting an obstacle, another grounded box, or the bottom of the board. If the amount of boxes directly on top of an obstacle exceeds its strength, then the obstacle is crushed under the pressure and the boxes above continue falling.
+
+Given board and strengths, your task is to return the state of the board after all boxes have fallen.
+
+Example
+
+    For
+
+board = {{'.', '#', '#', '#', '*'},
+         {'#', '#', '.', '#', '#'},
+         {'.', '.', '.', '#', '.'},
+         {'.', '.', '*', '*', '#'},
+         {'*', '#', '.', '.', '*'},
+         {'.', '*', '.', '.', '.'}}
+
+strengths = {{0, 0, 0, 0, 2},
+             {0, 0, 0, 0, 0},
+             {0, 0, 0, 0, 0},
+             {0, 0, 1, 1, 0},
+             {3, 0, 0, 0, 1},
+             {0, 2, 0, 0, 0}}
+
+the output should be
+
+fallAndCrush(board, strengths) = {{".", ".", ".", ".", "*"},
+                                  {".", ".", ".", ".", "."},
+                                  {".", ".", "#", ".", "."},
+                                  {"#", "#", "*", "#", "."},
+                                  {"*", "#", ".", "#", "#"},
+                                  {".", "#", ".", "#", "#"}}
+
+Example
+
+    For
+
+board = {{'#', '#', '#'},
+         {'#', '#', '#'},
+         {'#', '#', '#'},
+         {'.', '.', '.'},
+         {'*', '*', '*'},
+         {'*', '*', '*'},
+         {'*', '*', '*'}};
+
+strengths = {{0, 0, 0},
+             {0, 0, 0},
+             {0, 0, 0},
+             {0, 0, 0},
+             {1, 3, 2},
+             {2, 1, 3},
+             {3, 2, 1}}
+
+the output should be
+
+fallAndCrush(board, strengths) = {{".", ".", "."},
+                                  {".", "#", "."},
+                                  {".", "#", "#"},
+                                  {"#", "#", "#"},
+                                  {"#", "*", "#"},
+                                  {"#", "*", "*"},
+                                  {"*", "*", "*"}}
+
+Example
+
+Input/Output
+
+    {execution time limit} 0.5 seconds (cpp)
+
+    {input} array.array.char board
+
+    A matrix that shows where the boxes and obstacles are located. The matrix consists only of characters '.', '*', and '#'.
+
+    Guaranteed constraints:
+    3 ≤ board.length ≤ 100,
+    3 ≤ board{i}.length ≤ 100.
+
+    {input} array.array.integer strengths
+
+    A matrix representing the strengths of the obstacles. The matrix consists of non-negative integers.
+
+    Guaranteed constraints:
+    3 ≤ strengths.length ≤ 100,
+    3 ≤ strengths{i}.length ≤ 100,
+    0 ≤ strengths{i}{j} ≤ 100.
+
+    {output} array.array.char
+
+    Return a matrix representing the state of the board after all of the boxes have fallen.
+*/
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<vector<char>> fallAndCrush(vector<vector<char>> board, vector<vector<int>> strengths) {
+	int n = board.size();
+	int m = board[0].size();
+	int h = 0;
+
+	vector<vector<char>> ans(n, vector<char>(m, '.'));
+
+	for (int j = 0; j < m; ++j) {
+		h = 0;
+		for (int i = 0; i < n; ++i) {
+			if (board[i][j] == '#')
+				++h;
+			else if (board[i][j] == '*') {
+				if (h > 0 && h <= strengths[i][j])
+				{
+					int x = i;
+					if (h <= strengths[i][j]) {
+						ans[i][j] = '*';
+						--x;
+					}
+
+					for (; x >= 0; --x)
+					{
+						if (h > 0) {
+							ans[x][j] = '#';
+							--h;
+						}
+						else if (h == 0) {
+							if (board[x][j] == '*')
+							{
+								ans[x][j] = '*';
+								break;
+							}
+						}
+					}
+					h = 0;
+				}
+				else
+					ans[i][j] = '*';
+			}
+			if (i == n-1 && h > 0)
+			{
+				for (int x = i; x >= 0; --x)
+				{
+					if (h > 0) {
+						ans[x][j] = '#';
+						--h;
+					}
+					else if (h == 0) {
+						if (board[x][j] == '*')
+						{
+							ans[x][j] = '*';
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return ans;
+}
+
+int main()
+{
+	vector<int> arr1 = { 1, 5, 4, 3, 2, 8 };
+	vector<vector<int>> pieces1 = { {1, 5, 5},{3, 2},{8} };*/
+	
+	vector<int> arr2 = { 1, 2, 5, 3 };
+	vector<vector<int>> pieces2 = { {5},{1, 2},{3} };
+
+	cout << shuffleThePieces(arr1, pieces1) << endl;
+	cout << shuffleThePieces(arr2, pieces2) << endl;
+	return 0;
+}
---
+++ shuffleThePieces.cpp
@@ -0,0 +1,104 @@
+/*Your task to check whether it is possible to construct a given array of integers from a bunch of given pieces.
+
+More formally, you are given an array of distinct non-negative integers arr and an array of integer arrays pieces. Your task is to check whether it is possible to arrange the arrays of pieces in such a way that they can be concatenated to form an array equal to arr (containing all the same elements in the same order).
+
+Example
+
+    For arr = [1, 2, 5, 3] and pieces = [[5], [1, 2], [3]], the output should be shuffleThePieces(arr, pieces) = true.
+
+    The arrays of pieces can be arranged in the order [1, 2], [5], and [3], which would be equal to arr = [1, 2, 5, 3] when concatenated.
+
+    For arr = [1, 2, 5, 3, 6] and pieces = [[1, 2], [5], [6, 3]], the output should be shuffleThePieces(arr, pieces) = false.
+
+    example
+
+    There are no arrangements of pieces that would result in an array equal to arr.
+
+    For arr = [1, 5, 4, 3, 2, 8] and pieces = [[4, 3], [1, 5], [2]], the output should be shuffleThePieces(arr, pieces) = false.
+
+    Since the sum of the lengths of the arrays of pieces isn't equal to the length of arr, there's no way they could become equal, so the answer is false.
+
+    For arr = [1, 5, 4, 3, 2, 8] and pieces = [[1, 5, 5], [3, 2], [8]], the output should be shuffleThePieces(arr, pieces) = false.
+
+    Since the elements of arr aren't equal to the elements of the arrays of pieces, there's no way they could become equal, so the answer is false.
+
+Input/Output
+
+    [execution time limit] 0.5 seconds (cpp)
+
+    [input] array.integer arr
+
+    An array of pairwise distinct non-negative integers.
+
+    Guaranteed constraints:
+    1 ≤ arr.length ≤ 105,
+    0 ≤ arr[i] ≤ 109.
+
+    [input] array.array.integer pieces
+
+    An array of arrays of non-negative integers. The total count of integers in pieces doesn't exceed 105.
+
+    Guaranteed constraints:
+    1 ≤ pieces.length ≤ 105,
+    1 ≤ pieces[i].length ≤ 105,
+    0 ≤ pieces[i][j] ≤ 109.
+
+    [output] boolean
+
+    Return true if it's possible to rearrange the arrays of pieces and concatenate them, such that the resulting array is equal to arr, otherwise return false.
+*/
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool search(vector<int> piece, vector<int> arr)
+{
+	int index = -1;
+	for (int i = 0; i < arr.size(); i++)
+	{
+		if (piece[0] == arr[i])
+		{
+			index = i;
+			break;
+		}
+	}
+	if (index == -1)
+		return false;
+	for (int i = 0; i < piece.size(); i++)
+	{
+		if (arr[index] == piece[i])
+			index++;
+		else
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool shuffleThePieces(std::vector<int> arr, std::vector<std::vector<int>> pieces) {
+	size_t siz = 0;
+	for (size_t i = 0; i < pieces.size(); i++)
+	{
+		siz += pieces[i].size();
+	}
+	if (siz != arr.size())
+		return false;
+	for (int i = 0; i < pieces.size(); i++)
+	{
+		if (search(pieces[i], arr) == false)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+
+	return 0;
+}
+
